033cc00a385424599b45f198e5079257dbafde7c
abdel-dialo <diallo.abdoulgadirou@gmail.com>
2024-04-15 10:05:15 +0200

    delete aws access variable in terraform module

--- prod/main.tf
+++ prod/main.tf
@@ -1,8 +1,6 @@
 provider "aws" {
   region     = var.AWS_REGION
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
-}
+  }
 
 module "ec2_prod" {
     source = "../modules/ec2module"
